a1d597a205593bead16abf9a4e92efe7e67f2006
Ryocham <ryocham.prog@gmail.com>
2021-05-11 00:53:08 +0900

    Add 0009_PalindromeNumber project

---
+++ Easy/0009_PalindromeNumber/PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var solution = new Solution();
+            var x = 121;
+            solution.IsPalindrome(x);
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0) return false;
+
+            var strNum = x.ToString();
+            var len = strNum.Length;
+            if (len == 1) return true;
+
+            var reverseNum = string.Empty;
+            for (int i = len; i > 0; i--)
+            {
+                reverseNum += strNum.Substring(i - 1, 1);
+            }
+
+            if (strNum == reverseNum)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
