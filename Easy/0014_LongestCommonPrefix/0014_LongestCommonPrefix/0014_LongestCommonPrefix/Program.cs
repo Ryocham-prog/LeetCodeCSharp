0cf58c7beab4073ed302eeb2151a04bca39e50ab
Ryocham <ryocham.prog@gmail.com>
2021-05-14 02:04:35 +0900

    Add 0020_ValidParentheses project and Update LongestCommonPrefix

--- Easy/0014_LongestCommonPrefix/0014_LongestCommonPrefix/0014_LongestCommonPrefix/Program.cs
+++ Easy/0014_LongestCommonPrefix/0014_LongestCommonPrefix/0014_LongestCommonPrefix/Program.cs
@@ -11,14 +11,49 @@ namespace _0014_LongestCommonPrefix
             Console.WriteLine("Hello World!");
 
             var loncompre = new Program();
+            //var strs = new string[1] { "a" };
+            //var strs = new string[2] { "ab", "a" };
+            //var strs = new string[2] { "aa", "a" };
             var strs = new string[3] {"flower", "flow", "flight"};
-            loncompre.LongestCommonPrefix(strs);
+            //var strs = new string[3] { "dog", "racecar", "car" };
+            loncompre.LongestCommonPrefix2(strs);
         }
 
         // Runtime: 120ms
         public string LongestCommonPrefix(string[] strs)
         {
             if (strs.Length == 1) return strs[0];
+
+            var minLen = 200;
+            var searchStr = string.Empty;
+            foreach (string str in strs)
+            {
+                if (str.Length < minLen)
+                {
+                    minLen = str.Length;
+                    searchStr = str;
+                }
+            }
+            var checkStr = searchStr.Substring(0, minLen - 0);
+
+            var commonPrefix = string.Empty;
+            var searchword = string.Empty;
+            var searchPrefix = new StringBuilder();
+            foreach (var letter in searchStr)
+            {
+                searchword = searchPrefix.Append(letter).ToString();
+                if (!strs.All(x => x.StartsWith(searchword))) break;
+
+                commonPrefix = searchword;
+            }
+            return commonPrefix;
+        }
+
+        // Runtime: 96ms
+        public string LongestCommonPrefix2(string[] strs)
+        {
+            var strCount = strs.Length;
+            if (strCount == 1) return strs[0];
 
             var minLen = 200;
             var minStr = string.Empty;
@@ -31,17 +66,26 @@ namespace _0014_LongestCommonPrefix
                 }
             }
 
-            var commonPrefix = string.Empty;
-            var checkStr = string.Empty;
-            var targetPrefix = new StringBuilder();
+            if (minLen == 0) return string.Empty;
+
+            var commonPrefix = new StringBuilder();
+            var idx = 0;
             foreach (var letter in minStr)
             {
-                checkStr = targetPrefix.Append(letter).ToString();
-                if (!strs.All(x => x.StartsWith(checkStr))) break;
-
-                commonPrefix = checkStr;
+                var checkFlag = true;
+                for (int i = 0; i < strCount; i++)
+                {
+                    if (!(strs[i].Substring(idx, 1) == letter.ToString()))
+                    {
+                        checkFlag = false;
+                        break;
+                    }
+                }
+                if (!checkFlag) break;
+                commonPrefix.Append(letter);
+                idx++;
             }
-            return commonPrefix;
+            return commonPrefix.ToString();
         }
     }
 }
---
+++ Easy/0020_ValidParentheses/ValidParentheses/ValidParentheses/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var isval = new Program();
+            var s = "([])";
+            var rtn = isval.IsValid(s);
+        }
+
+        // Runtime: 124ms
+        public bool IsValid(string s)
+        {
+            if (s.Length % 2 == 1) return false;
+
+            var targetBefore = s;
+            var targetAfter = string.Empty;
+            var i = 0;
+            while (targetBefore.Length != targetAfter.Length)
+            {
+                if (i != 0) targetBefore = targetAfter;
+
+                targetAfter = targetBefore.Replace("()", "");
+                targetAfter = targetAfter.Replace("[]", "");
+                targetAfter = targetAfter.Replace("{}", "");
+
+                i++;
+            }
+            return targetAfter.Length == 0;
+        }
+
+        // Runtime: 152ms
+        public bool IsValid2(string s)
+        {
+            if (s.Length % 2 == 1) return false;
+            var target = s;
+
+            while (target.IndexOf("()") > -1 || target.IndexOf("[]") > -1 || target.IndexOf("{}") > -1)
+            {
+                target = target.Replace("()", "").Replace("[]", "").Replace("{}", "");
+            }
+
+            return target.Length == 0;
+        }
+
+        // Runtime: 128ms
+        public bool IsValid3(string s)
+        {
+            if (s.Length % 2 == 1) return false;
+            var target = s;
+
+            var isLoop = true;
+            while (isLoop == true)
+            {
+                var beforeTarget = target;
+                target = target.Replace("()", "").Replace("[]", "").Replace("{}", "");
+
+                if (beforeTarget == target) isLoop = false;
+            }
+            return target.Length == 0;
+        }
+
+    }
+}
