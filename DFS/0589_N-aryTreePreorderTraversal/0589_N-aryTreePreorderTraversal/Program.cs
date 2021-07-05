c500d9d5ce0a827884ed70b7a7d53bab34948531
Ryocham <ryocham.prog@gmail.com>
2021-07-06 01:47:59 +0900

    Add 0589_N-aryTreePreorderTraversal project

---
+++ DFS/0589_N-aryTreePreorderTraversal/0589_N-aryTreePreorderTraversal/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,IList<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+public class Solution {
+    public IList<int> Preorder(Node root) {
+        var result = new List<int>();
+        
+        DFS(root, ref result);
+            
+        return result;
+    }
+    
+    private void DFS(Node node, ref List<int> result)
+    {
+        if(node == null) return;
+        result.Add(node.val);
+
+        foreach(var child in node.children)
+        {
+            DFS(child, ref result);
+        }
+    }
+}
