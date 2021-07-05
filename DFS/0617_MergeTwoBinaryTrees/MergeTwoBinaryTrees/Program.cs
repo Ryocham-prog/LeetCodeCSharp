492fc0faf39a91b8c4cc3927bad9289d61dc4927
Ryocham <ryocham.prog@gmail.com>
2021-07-06 01:46:55 +0900

    Modify 0617_MergeTwoBinaryTrees project

--- DFS/0617_MergeTwoBinaryTrees/MergeTwoBinaryTrees/Program.cs
+++ DFS/0617_MergeTwoBinaryTrees/MergeTwoBinaryTrees/Program.cs
@@ -16,9 +16,9 @@ public class Solution {
         if(root1 == null) return root2;
         if(root2 == null) return root1;
         
-        root1.val += root2.val;
-        root1.left = MergeTrees(root1.left, root2.left);
-        root1.right = MergeTrees(root1.right, root2.right);
-        return root1;
+        var result = new TreeNode(root1.val + root2.val);
+        result.left = MergeTrees(root1.left, root2.left);
+        result.right = MergeTrees(root1.right, root2.right);
+        return result;
     }
 }
