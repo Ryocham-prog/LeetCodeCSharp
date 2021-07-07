382b9d5eb638d55822db8602f62b975c2887d3c9
Ryocham <ryocham.prog@gmail.com>
2021-07-08 02:21:42 +0900

    Add 1022_SumOfRootToLeafBinaryNumbers project

---
+++ DFS/1022_SumOfRootToLeafBinaryNumbers/1022_SumOfRootToLeafBinaryNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int SumRootToLeaf(TreeNode root) {
+        if(root == null) return 0;
+        
+        var result = 0;
+        var tmp = String.Empty;
+        
+        DFS(root, ref result, tmp);
+        
+        return result;
+    }
+    
+    private void DFS(TreeNode node, ref int result, string tmp)
+    {
+        if(node == null) return;
+        tmp += node.val.ToString();
+        
+        if(node.left == null && node.right == null) {
+            result += Convert.ToInt32(tmp, 2);
+            return;
+        }
+        
+        DFS(node.left, ref result, tmp);
+        DFS(node.right, ref result, tmp);
+           
+    }
+}
