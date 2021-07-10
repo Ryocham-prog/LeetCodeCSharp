1f0435840140c2709d5dc38651963d33bb3c90a8
Ryocham <ryocham.prog@gmail.com>
2021-07-11 03:16:08 +0900

    Add 0226_InvertBinaryTree project

---
+++ DFS/0226_InvertBinaryTree/0226_InvertBinaryTree/Program.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        var result = root;
+        if(root == null) return null;
+
+        var dummyR = InvertTree(root.right);
+        var dummyL = InvertTree(root.left);
+        
+        result.left = dummyR;
+        result.right = dummyL;
+        
+        return result;
+    }
+}
