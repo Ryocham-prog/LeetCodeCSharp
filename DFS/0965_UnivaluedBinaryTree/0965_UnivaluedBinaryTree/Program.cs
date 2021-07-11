40a73dab9a5d2b39ffc499a119cba14475964cfc
Ryocham <ryocham.prog@gmail.com>
2021-07-11 23:00:20 +0900

    Add 0965_UnivaluedBinaryTree project

---
+++ DFS/0965_UnivaluedBinaryTree/0965_UnivaluedBinaryTree/Program.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsUnivalTree(TreeNode root) {
+        if(root.left == null && root.right == null) return true;
+
+        var valList = new List<int>();
+        DFS(root, valList);
+        
+        return valList.GroupBy(x => x).Count() == 1;
+    }
+    
+    private void DFS(TreeNode node, List<int> valList)
+    {
+        if(node == null) return;
+        valList.Add(node.val);
+        
+        DFS(node.left, valList);
+        
+        DFS(node.right, valList);
+    }
+}
