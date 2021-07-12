cf7c4d45d8c64c4478b58a54d30f72d978bdd435
Ryocham <ryocham.prog@gmail.com>
2021-07-13 01:43:49 +0900

    Add 0094_BinaryTreeInorderTraversal project

---
+++ DFS/0094_BinaryTreeInorderTraversal/0094_BinaryTreeInorderTraversal/Program.cs
@@ -0,0 +1,33 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> InorderTraversal(TreeNode root) {
+        var result = new List<int>();
+        if(root == null) return result;
+        
+        DFS(root, result);
+        
+        return result;
+    }
+    
+    private void DFS(TreeNode node, List<int> result)
+    {
+        if(node == null) return;
+        
+        DFS(node.left, result);
+        result.Add(node.val);
+
+        DFS(node.right, result);
+    }
+}
