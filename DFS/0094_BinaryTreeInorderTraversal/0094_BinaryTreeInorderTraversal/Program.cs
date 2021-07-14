a3c8a9891b2d1dd4d12d2a1ba0bd665842a0bcbf
Ryocham <ryocham.prog@gmail.com>
2021-07-15 00:36:27 +0900

    Modify 0094_BinaryTreeInorderTraversal project and Modify 0463_IslandPerimeter project

--- DFS/0094_BinaryTreeInorderTraversal/0094_BinaryTreeInorderTraversal/Program.cs
+++ DFS/0094_BinaryTreeInorderTraversal/0094_BinaryTreeInorderTraversal/Program.cs
@@ -11,6 +11,7 @@
  *     }
  * }
  */
+ // voidパターン(428 ms)
 public class Solution {
     public IList<int> InorderTraversal(TreeNode root) {
         var result = new List<int>();
@@ -29,5 +30,29 @@ public class Solution {
         result.Add(node.val);
 
         DFS(node.right, result);
+    }
+}
+// ------------------------------------------------------
+
+ // 戻り値Listパターン(240 ms)
+public class Solution {
+    public IList<int> InorderTraversal(TreeNode root) {
+        var result = new List<int>();
+        if(root == null) return result;
+        
+        result = DFS(root, result);
+        
+        return result;
+    }
+    
+    private List<int> DFS(TreeNode node, List<int> result)
+    {
+        if(node == null) return result;
+        var ret = new List<int>(result);
+        
+        ret = DFS(node.left, result);
+        ret.Add(node.val);
+        ret = DFS(node.right, result);
+        return ret;
     }
 }
--- DFS/0463_IslandPerimeter/0463_IslandPerimeter/Program.cs
+++ DFS/0463_IslandPerimeter/0463_IslandPerimeter/Program.cs
@@ -1,4 +1,5 @@
 ﻿public class Solution {
+    // DFSではない解き方(192 ms)
     public int IslandPerimeter(int[][] grid) {
         var result = 0;
         for (var row = 0; row < grid.GetLength(0); row++)
@@ -14,5 +15,61 @@
             }
         }
         return result;
+    }
+
+// -------------------------------------------------------
+
+    // DFS(180 ms)
+        public int IslandPerimeter(int[][] grid) {
+        var result = 0;
+        
+        var row = grid.Length;
+        var col = grid[0].Length;
+        
+        for (var i = 0; i < row; i++){
+            for (var j = 0; j < col; j++){
+                if (grid[i][j] == 1){
+                    result += DFS(grid, i, j, row, col);
+                }
+            }
+        }
+        return result;
+    }
+    
+    private int DFS(int[][] grid, int i, int j, int row, int col){
+        int res = 0;
+        if(i >= 0 && i < row && j >= 0 && j < col && grid[i][j] == 1){
+            grid[i][j] = -1;
+            
+            if(i == 0) // 最上部
+                res++;
+            
+            if(i == row - 1) // 最下部
+                res++;
+            
+            if(j == 0) // 最左
+                res++;
+            
+            if(j == col - 1) // 最右
+                res++;
+            
+            if(i > 0 && grid[i - 1][j] == 0) // 上部
+                res++;
+            
+            if(i < row - 1 && grid[i + 1][j] == 0) // 下部
+                res++;
+            
+            if(j > 0 && grid[i][j - 1] == 0) // 左
+                res++;
+            
+            if(j < col - 1 && grid[i][j + 1] == 0) // 右
+                res++;
+            
+            res += DFS(grid, i + 1, j, row, col);
+            res += DFS(grid, i - 1, j, row, col);
+            res += DFS(grid, i, j + 1, row, col);
+            res += DFS(grid, i, j - 1, row, col);
+        }
+        return res;
     }
 }
