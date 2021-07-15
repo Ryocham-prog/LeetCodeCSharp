2cb670f9b6c683da0b107e115af22e3fea4a436e
Ryocham <ryocham.prog@gmail.com>
2021-07-16 02:37:20 +0900

    Add 0690_EmployeeImportance project

---
+++ DFS/0690_EmployeeImportance/0690_EmployeeImportance/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public IList<int> subordinates;
+}
+*/
+
+class Solution {
+    public int GetImportance(IList<Employee> employees, int id) {
+        if(employees == null) return 0;
+        
+        var result = 0;
+        foreach(var employee in employees)
+        {
+            if(employee.id != id) continue;
+            
+            result += employee.importance;
+            
+            if(employee.subordinates == null) break;
+            
+            var subordinates = employee.subordinates;
+            foreach(var sub in subordinates)
+            {
+                result += GetImportance(employees, sub);
+            }
+            break;
+        }       
+        return result;
+    }
+}
