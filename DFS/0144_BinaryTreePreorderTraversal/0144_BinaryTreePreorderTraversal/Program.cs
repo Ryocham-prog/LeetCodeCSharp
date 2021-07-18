6e47f0e6d44b5d1a22194b64d0aa2055ae568206
Ryocham <ryocham.prog@gmail.com>
2021-07-19 00:44:39 +0900

    Add 0144_BinaryTreePreorderTraversal project

---
+++ DFS/0144_BinaryTreePreorderTraversal/0144_BinaryTreePreorderTraversal/Program.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<int> PreorderTraversal(TreeNode root) {
+        var result = new List<int>();
+        if(root == null) return result;
+        
+        result = DFS(root, result);
+        
+        return result;
+    }
+    
+    private List<int> DFS(TreeNode node, List<int> tmp)
+    {
+        if(node == null) return tmp;
+        var result = new List<int>(tmp);
+
+        result.Add(node.val);
+        
+        result = DFS(node.left, result);
+        result = DFS(node.right, result);
+        
+        return result;
+    }
+}
