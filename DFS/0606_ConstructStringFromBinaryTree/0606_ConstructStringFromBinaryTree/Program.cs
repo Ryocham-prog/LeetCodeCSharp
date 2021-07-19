3309b52a29fd9b8bdd5db0d51cb0521c7d476490
Ryocham <ryocham.prog@gmail.com>
2021-07-20 02:42:52 +0900

    Add 0606_ConstructStringFromBinaryTree project

---
+++ DFS/0606_ConstructStringFromBinaryTree/0606_ConstructStringFromBinaryTree/Program.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public string Tree2str(TreeNode root) {
+        var result = new StringBuilder();
+        result = DFS(root, result);
+        
+        return result.ToString();
+    }
+    
+    private StringBuilder DFS(TreeNode node, StringBuilder tmp)
+    {
+        if(node == null) return tmp;
+        
+        tmp.Append($"{node.val}");
+        
+        if(node.left == null && node.right == null) return tmp;
+        
+        tmp.Append("(");
+        tmp = DFS(node.left, tmp);
+        tmp.Append(")");
+        
+        if(node.right != null)
+        {
+            tmp.Append("(");
+            tmp = DFS(node.right, tmp);
+            tmp.Append(")");
+        }
+        return tmp;
+    }
+}
