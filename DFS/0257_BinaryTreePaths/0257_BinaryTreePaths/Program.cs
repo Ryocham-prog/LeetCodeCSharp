3796f74680248b7fedc2fcafb32573fa4204b6e6
Ryocham <ryocham.prog@gmail.com>
2021-07-22 01:34:03 +0900

    Add 0257_BinaryTreePaths project

---
+++ DFS/0257_BinaryTreePaths/0257_BinaryTreePaths/Program.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<string> BinaryTreePaths(TreeNode root) {
+        var result = new List<string>();
+        if(root == null) return result;
+        
+        result = DFS(root, result, "");
+        
+        return result;
+    }
+    
+    private List<string> DFS(TreeNode node, List<string> tmpList, string tmpStr)
+    {
+        if(node == null) return tmpList;
+        
+        var resList = new List<string>(tmpList);
+        var resStr = tmpStr + node.val;
+        
+        if(node.left == null && node.right == null) 
+        {
+            resList.Add(resStr);
+            return resList;
+        }
+        
+        resStr = resStr + "->";
+        
+        resList = DFS(node.left, resList, resStr);
+        resList = DFS(node.right, resList, resStr);
+        
+        return resList;
+    }
+}
