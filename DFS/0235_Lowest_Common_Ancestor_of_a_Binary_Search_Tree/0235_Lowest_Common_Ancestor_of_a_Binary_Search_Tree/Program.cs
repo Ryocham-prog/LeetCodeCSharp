c83778caa9c330bfd3e5a0ff09c85d6c8ee85760
Ryocham <ryocham.prog@gmail.com>
2021-07-25 02:38:36 +0900

    Add 0235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree project

---
+++ DFS/0235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/0235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Program.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        int parentVal = root.val;
+        int pVal = p.val;
+        int qVal = q.val;
+
+        if (pVal > parentVal && qVal > parentVal) {
+            return LowestCommonAncestor(root.right, p, q);
+        } else if (pVal < parentVal && qVal < parentVal) {
+            return LowestCommonAncestor(root.left, p, q);
+        }
+        
+        return root;
+    }
+}
