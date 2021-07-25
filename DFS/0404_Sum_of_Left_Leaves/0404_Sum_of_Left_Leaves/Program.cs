87530955d357ec7ec2a9ceeb2487043d10863099
Ryocham <ryocham.prog@gmail.com>
2021-07-26 01:45:48 +0900

    Add 0404_Sum_of_Left_Leaves project

---
+++ DFS/0404_Sum_of_Left_Leaves/0404_Sum_of_Left_Leaves/Program.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int SumOfLeftLeaves(TreeNode root) {
+        if(root == null) return 0;
+        
+        var resultList = new List<int>();
+        resultList = DFS(root, false, resultList);
+        
+        return resultList.Sum();
+    }
+    
+    private List<int> DFS(TreeNode node, bool isLeft, List<int> tempList)
+    {
+        if (node == null) return tempList;
+        var resList = new List<int>(tempList);
+        
+        if (node.left == null && node.right == null) {
+            if(isLeft) resList.Add(node.val);
+            return resList;
+        }
+        
+        resList = DFS(node.left, true, resList);
+        resList = DFS(node.right, false, resList);
+        
+        return resList;
+    }
+}
