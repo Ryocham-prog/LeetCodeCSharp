963052f6e2a9b73a266db979ab7a17a81613e58a
Ryocham <ryocham.prog@gmail.com>
2021-07-28 22:30:50 +0900

    Add 0993_Cousins_in_Binary_Tree project

---
+++ DFS/0993_Cousins_in_Binary_Tree/0993_Cousins_in_Binary_Tree/Program.cs
@@ -0,0 +1,39 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsCousins(TreeNode root, int x, int y) {
+        if(root == null) return false;
+
+        var infoList = new List<int>();
+        infoList = DFS(root, 0, 0, x, y, infoList);
+
+        if(infoList.Count < 4) return false;
+
+        return ((infoList[0] == infoList[2]) && (infoList[1] != infoList[3]));
+    }
+    
+    private List<int> DFS(TreeNode node, int nowDepth, int nowParentVal, int x, int y, List<int> infoList)
+    {
+        if(node == null || infoList.Count == 4 ) return infoList;
+
+        var tmpList = new List<int>(infoList);
+        if(node.val == x || node.val == y)
+        {
+            tmpList.Add(nowDepth);
+            tmpList.Add(nowParentVal);
+        }
+        
+        return DFS(node.right, nowDepth + 1, node.val, x, y, DFS(node.left, nowDepth + 1, node.val,  x, y, tmpList));
+    }
+}
