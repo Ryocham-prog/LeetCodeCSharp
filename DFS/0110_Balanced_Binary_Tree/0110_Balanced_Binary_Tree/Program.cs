e76c22aa699647de7f5eaef16d17fd4e4f21db6b
Ryocham <ryocham.prog@gmail.com>
2021-08-01 21:52:56 +0900

    Add 0110_Balanced_Binary_Tree project

---
+++ DFS/0110_Balanced_Binary_Tree/0110_Balanced_Binary_Tree/Program.cs
@@ -0,0 +1,35 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsBalanced(TreeNode root) {
+        if(root == null) return true;
+        
+        var result = true;
+        DFS(root, ref result);
+        
+        return result;
+    }
+    
+    private int DFS(TreeNode node, ref bool result)
+    {
+        if(node == null || result == false) return 0;
+        
+        var leftDepth = DFS(node.left, ref result);
+        var rightDepth = DFS(node.right, ref result);
+        
+        if(Math.Abs(leftDepth - rightDepth) > 1) result = false;
+        
+        return Math.Max(leftDepth, rightDepth) + 1;
+    }
+}
