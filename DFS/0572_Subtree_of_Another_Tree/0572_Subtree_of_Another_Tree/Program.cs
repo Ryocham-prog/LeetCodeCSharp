10185dda7d2173cf7e9b50e3a274471d127a8948
Ryocham <ryocham.prog@gmail.com>
2021-08-04 00:59:11 +0900

    Add 0572_Subtree_of_Another_Tree project

---
+++ DFS/0572_Subtree_of_Another_Tree/0572_Subtree_of_Another_Tree/Program.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) return false;
+        
+        if (root.val == subRoot.val && DFS(root, subRoot)) return true;
+        
+        return IsSubtree(root.left, subRoot) || IsSubtree(root.right, subRoot);
+    }
+    
+    private bool DFS(TreeNode node, TreeNode subNode)
+    {
+        if(node == null && subNode == null) return true;
+        
+        if((node != null && subNode == null) || (node == null && subNode != null)) return false;
+        
+        return node.val == subNode.val && DFS(node.left, subNode.left) && DFS(node.right, subNode.right);
+    }
+}
