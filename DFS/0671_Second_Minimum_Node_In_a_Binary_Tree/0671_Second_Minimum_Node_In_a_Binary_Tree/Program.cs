9b4672362bc307e304c19410888b945601df0449
Ryocham <ryocham.prog@gmail.com>
2021-08-05 22:05:02 +0900

    Add 0671_Second_Minimum_Node_In_a_Binary_Tree project

---
+++ DFS/0671_Second_Minimum_Node_In_a_Binary_Tree/0671_Second_Minimum_Node_In_a_Binary_Tree/Program.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int FindSecondMinimumValue(TreeNode root) {
+        if(root == null) return -1;
+        
+        var resultList = DFS(root);
+        
+        var result = resultList
+            .Distinct()
+            .OrderBy(x => x)
+            .ElementAtOrDefault(1);
+        
+        if(result == 0) return -1;
+        return result;
+    }
+    
+    private List<int> DFS(TreeNode node)
+    {
+        if(node == null) return new List<int>();
+        
+        var tmpList = new List<int>();
+        tmpList.Add(node.val);
+     
+        tmpList.AddRange(DFS(node.left));
+        tmpList.AddRange(DFS(node.right));
+        
+        return tmpList;
+    }
+}
